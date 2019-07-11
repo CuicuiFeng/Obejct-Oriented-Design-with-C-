74395859a3081e98fd4388c253c233e0f6c72501
Cuicui Feng <feng.cu@husky.neu.edu>
2019-07-11 15:38:53 -0400

    daycare_mvc

---
+++ DayCare/DayCare/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DayCare.Models;
+
+namespace DayCare.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            //create object model
+            RosterModel rosterModel = new RosterModel()
+            {
+                FirstName = "Tracy",
+                LastName = "Feng",
+                Age = 23,
+                Id = 001
+            };
+            return View(rosterModel);
+            //return Content("Hello World!");
+            //return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DayCare/DayCare/Models/RosterModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace DayCare.Models
+{
+    public class RosterModel
+    {
+        public RosterModel()
+        {
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int Id { get; set; }
+
+    }
+}
